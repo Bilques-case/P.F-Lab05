fae70d34a08e62ee46455e19f474831c40970a12
Bilques-case <arhumbilques@gmail.com>
2024-10-24 23:37:00 +0500

    Add files via upload

---
+++ Q 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //Lab 05 question 01
+int i;
+cout<<"The first 5 natural numbers are\n";
+for (i=1;i<=5;i++){
+    cout<<i<<" "; 
+}
+
+    return 0;
+}
